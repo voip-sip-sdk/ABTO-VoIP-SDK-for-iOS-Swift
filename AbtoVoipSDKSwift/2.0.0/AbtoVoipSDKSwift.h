5687bf2fa7fca2d8173ffc7593f0fa96592793cc
voip-sip-sdk <45635898+voip-sip-sdk@users.noreply.github.com>
2026-04-23 01:00:41 +0300

    Update AbtoVoipSDKSwift.h

--- AbtoVoipSDKSwift/2.0.0/AbtoVoipSDKSwift.h
+++ AbtoVoipSDKSwift/2.0.0/AbtoVoipSDKSwift.h
@@ -1,7 +1,6 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-//#import <AbtoSipPhoneObserver.h>
-#import <AbtoSipClientWrapper.framework/Headers/AbtoSipPhoneObserver.h>
+#import <AbtoSipClientWrapper/AbtoSipPhoneObserver.h>
 
 FOUNDATION_EXPORT double AbtoVoipSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char AbtoVoipSDKVersionString[];
